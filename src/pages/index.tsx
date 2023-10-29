9d46364a6a7931e89c3e8b72b67355a00687b6e5
John <john@sparkblox.com>
2023-10-29 14:16:29 -0700

    user auth from supabase added

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,9 @@
 import Image from 'next/image'
 import { supabase } from '../lib/supabase';
+import { useContext } from 'react';
+import { AuthContext } from '../contexts/auth';
+import { UserContext, Auth } from '@supabase/ui';
+
 
 export async function getServerSideProps() {
     let { data: topics, error } = await supabase.from('topics').select('*');
@@ -12,9 +16,21 @@ export async function getServerSideProps() {
   }
 
 export default function Home({ topics }) {
+  const { user, signIn, signOut } = useContext(AuthContext);
   return (
 	<div>
   	<h1>Welcome to the Debate Judging App!</h1>
+    {!user ? (
+            	<Auth supabaseClient={supabase}>
+                	<Auth.LogIn />
+                	<Auth.SignUp />
+            	</Auth>
+        	) : (
+            	<>
+                	<p>Hello, {user.email}</p>
+                	<button onClick={signOut}>Logout</button>
+            	</>
+        	)}
   	<ul>
     	{topics.map(topic => (
       	<li key={topic.id}>{topic.topic_name}</li>
